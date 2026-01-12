0ae3651907ce9504651dcef0dce1abb2bf8ca88a
Dorian <gaurondorian@gmail.com>
2026-01-11 21:43:23 -0500

    ♻️ base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://21spaces.github.io.github.io/stepsy-frontend",
   plugins: [vue(), tailwindcss()],
   optimizeDeps: {
     include: ["@apollo/client/core"],
